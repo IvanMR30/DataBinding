ed86b5953d84849d516b55337130375c37009944
Ivan Alberto Morales Rosas <24470026@campeche.tecnm.mx>
2025-10-29 11:46:43 -0600

    Ejemplo de sincronizacion de elementos de XAML con manejo de eventos

---
+++ DataBinding.XamlElement/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DataBinding.XamlElement
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            EnteredTextLabel.Text=string.Empty;
+        }
+        private void Entry_Changed_TextChanged(object sender, EventArgs e) {
+            EnteredTextLabel.Text= TextEntry.Text;
+        }
+
+    }
+}
