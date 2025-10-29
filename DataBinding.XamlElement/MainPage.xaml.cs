acd24ba2457971ee927e385e9e420b9a5ed7d3c0
Ivan Alberto Morales Rosas <24470026@campeche.tecnm.mx>
2025-10-29 11:58:26 -0600

    Ejemplo de enlace de datos a un elemento XAML

--- DataBinding.XamlElement/MainPage.xaml.cs
+++ DataBinding.XamlElement/MainPage.xaml.cs
@@ -5,10 +5,7 @@
         public MainPage()
         {
             InitializeComponent();
-            EnteredTextLabel.Text=string.Empty;
-        }
-        private void Entry_Changed_TextChanged(object sender, EventArgs e) {
-            EnteredTextLabel.Text= TextEntry.Text;
+          
         }
 
     }
