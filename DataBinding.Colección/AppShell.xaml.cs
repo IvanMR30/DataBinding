4f5e8b0970a23a113f8dead39013f0fb9c886d74
Ivan Alberto Morales Rosas <24470026@campeche.tecnm.mx>
2025-11-12 12:35:52 -0600

    Ejemplo de una colección de objetos sin usar databinding

---
+++ DataBinding.Colección/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DataBinding.Colección
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataBinding.Colección/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DataBinding.Colección")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DataBinding.Colección.Pages")]
